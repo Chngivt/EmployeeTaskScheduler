65d661d1350048bb2dcdc38c008c0046fd09fe3d
chngvit <166533354+Chngivt@users.noreply.github.com>
2026-08-03 19:31:05 +0700

    Update schedule.py

--- app/models/schedule.py
+++ app/models/schedule.py
@@ -1,14 +1,10 @@
 from app import db
-from datetime import datetime
 
 class Schedule(db.Model):
     __tablename__ = 'schedule'
-    
     id = db.Column(db.Integer, primary_key=True)
-    # Khóa ngoại liên kết tới bảng Employee và Task
-    is_overtime = db.Column(db.Boolean, default=False)
+    is_overtime = db.Column(db.Boolean, default=False) # Cờ đánh dấu tăng ca
     employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=False)
     task_id = db.Column(db.Integer, db.ForeignKey('task.id'), nullable=False)
-    
     date = db.Column(db.Date, nullable=False)
-    shift = db.Column(db.String(20), nullable=False) # 'Sáng' hoặc 'Chiều' hoặc 'Tối'
+    shift = db.Column(db.String(20), nullable=False)
