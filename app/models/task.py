d48d2f20d0d23f35dad9a890d1065d393123a08a
chngvit <166533354+Chngivt@users.noreply.github.com>
2026-08-03 18:19:33 +0700

    Update task.py

--- app/models/task.py
+++ app/models/task.py
@@ -6,6 +6,7 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     code = db.Column(db.String(20), unique=True, nullable=False)
     task_name = db.Column(db.String(200), nullable=False)
+    wage = db.Column(db.Integer, default=150000)
     priority = db.Column(db.Integer, nullable=False) # Ví dụ: 1-Cao, 2-TB, 3-Thấp
     duration = db.Column(db.Integer, nullable=False) # Thời lượng (giờ)
     
